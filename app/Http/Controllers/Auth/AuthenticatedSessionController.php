584267b9bba56cc066513d2f224fb6dd9b2ad765
afiq <nurafiq@utm.my>
2025-05-18 19:52:38 +0800

    redirect for role

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -24,11 +24,23 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        // dd($request);
+
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $url = '';
+        if($request->user()->role === 'admin')
+        {
+            $url = '/admin/dashboard';
+        } elseif($request->user()->role === 'user')
+        {
+            $url = '/dashboard';
+        }
+
+        // return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended($url);
     }
 
     /**
