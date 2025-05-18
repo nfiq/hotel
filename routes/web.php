00933c3ccbea352841a1c284c7dbbd23bb73ca8e
afiq <nurafiq@utm.my>
2025-05-18 21:29:10 +0800

    add middleware for role user

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+//Admin group middleware
+Route::middleware(['auth', 'roles:admin'])->group(function(){
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+}); //end admin middkeware
+
 
